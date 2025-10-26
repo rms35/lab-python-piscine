73079da72e2345bcfd0278effd61d3152a77a94d
rms35 <martinsoeder35@gmail.com>
2025-10-26 11:23:13 +0100

    ex00 terminado

--- ex00/hola.py
+++ ex00/hola.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
-import sys
-
-print(str.upper.__doc__)
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+ft_list.remove("tata!")
+ft_list.insert(1, "World")
+ft_tuple = ("Hello", "Spain!")
+ft_set.remove("tutu!")
+ft_set.add("Madrid!")
+ft_dict["Hello"] = "42Madrid!"
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
