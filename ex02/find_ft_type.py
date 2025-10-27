03cd8b68d7e214410ed64c5e89f59ad34f3016fb
rms35 <martinsoeder35@gmail.com>
2025-10-27 13:12:47 +0100

    Protegydo con try except

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,14 +1,17 @@
-def all_thing_is_obj(object: any) -> int:
-    if isinstance(object, list):
-        print("List :", type(object))
-    elif isinstance(object, tuple):
-        print("Tuple :", type(object))
-    elif isinstance(object, set):
-        print("Set :", type(object))
-    elif isinstance(object, dict):
-        print("Dict :", type(object))
-    elif isinstance(object, str):
-        print(f"{object} is in the kitchen :", type(object))
-    else:
-        print("Type not found")
-    return 42
+def	all_thing_is_obj(object: any) -> int:
+	try:
+		if isinstance(object, list):
+			print("List :", type(object))
+		elif isinstance(object, tuple):
+			print("Tuple :", type(object))
+		elif isinstance(object, set):
+			print("Set :", type(object))
+		elif isinstance(object, dict):
+			print("Dict :", type(object))
+		elif isinstance(object, str):
+			print(f"{object} is in the kitchen :", type(object))
+		else:
+			print("Type not found")
+	except Exception as e:
+		print(f"Error: {e}")
+	return 42
