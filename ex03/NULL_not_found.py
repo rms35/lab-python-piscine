c638fb6448ac6f66d5a4ea6a447d03a28ecc1f11
rms35 <martinsoeder35@gmail.com>
2025-10-27 14:22:33 +0100

    creo que ex03 terminado

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,12 +1,22 @@
 def	NULL_not_found(object: any) -> int:
-	r = type(object)
+	r = None
 	try:
-		if r is None:
-			print("{None}: ", {type(object)})
-		elif isinstance(object, ):
-			pass
-		else:
-			print("asd")
+		match object:
+			case None:
+				r = 'Nothing: '
+			case float() if object != object:
+				r = 'Cheese: '
+			case bool() if object is False:
+				r = 'Fake: '
+			case int() if object == 0:
+				r = 'Zero: '
+			case str() if not object:
+				r = 'Empty: '
+			case _:
+				print('Type not found')
+				return 1
+		if r is not None:
+			print(f'{r} {object} {type(object)}')
 	except Exception as e:
 		print("Error: {e}")
 		return 1
--- ex03/tester.py
+++ ex03/tester.py
@@ -6,8 +6,8 @@ Zero = 0
 Empty = ''
 Fake = False
 NULL_not_found(Nothing)
-# NULL_not_found(Garlic)
-# NULL_not_found(Zero)
-# NULL_not_found(Empty)
-# NULL_not_found(Fake)
-# print(NULL_not_found("Brian"))
+NULL_not_found(Garlic)
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Fake)
+print(NULL_not_found("Brian"))
