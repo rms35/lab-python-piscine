c4ba28288e5787dad680aead47fd517a01cc15b8
rms35 <martinsoeder35@gmail.com>
2025-10-27 12:33:14 +0100

    código un poco más claro

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -3,9 +3,9 @@
 from datetime import datetime
 try:
 	now = datetime.now()
-	t = datetime.fromtimestamp(0)
+	epoch = datetime.fromtimestamp(0)
 	ts = int(now.timestamp())
-	string = t.strftime("%B %d, %Y")
+	string = epoch.strftime("%B %d, %Y")
 	print(f"Seconds since {string}: {ts:,} or {ts:e} in scientific notation")
 	ts = now.date()
 	string = ts.strftime("%B %d, %Y")
