64b51794c9412a3bcd2f1521c78474947dbbcac6
rms35 <martinsoeder35@gmail.com>
2025-10-27 12:29:38 +0100

    prtegido contra errores

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-
-now = datetime.now()
-t = datetime.fromtimestamp(0)
-ts = int(now.timestamp())
-string = t.strftime("%B %d, %Y")
-print(f"Seconds since {string}: {ts:,} or {ts:e} in scientific notation")
-ts = now.date()
-string = ts.strftime("%B %d, %Y")
-print(string)
+try:
+	now = datetime.now()
+	t = datetime.fromtimestamp(0)
+	ts = int(now.timestamp())
+	string = t.strftime("%B %d, %Y")
+	print(f"Seconds since {string}: {ts:,} or {ts:e} in scientific notation")
+	ts = now.date()
+	string = ts.strftime("%B %d, %Y")
+	print(string)
+except Exception as e:
+	print(f"Error: {e}")
